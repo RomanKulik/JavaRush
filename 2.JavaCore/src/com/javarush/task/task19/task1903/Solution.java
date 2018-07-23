00a6c3347f2a55c5b770ab47a8aaea54b8305a4f
RomanKulik <roman.ser.kulik@gmail.com>
2018-07-23 14:34:45 +0300

    Adapter. MaskFormatter. String.format phonenNum Адаптация неск интерф IncomeData к Customer и Contact

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -0,0 +1,202 @@
+package com.javarush.task.task19.task1903;
+
+/* 
+Адаптация нескольких интерфейсов
+Адаптация нескольких интерфейсов
+Адаптируй IncomeData к Customer и Contact.
+Классом-адаптером является IncomeDataAdapter.
+Инициализируйте countries перед началом выполнения программы.
+Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+Дополнить телефонный номер нулями до 10 цифр при необходимости (смотри примеры).
+Обратите внимание на формат вывода фамилии и имени человека.
+
+
+Требования:
+1. Класс Solution должен содержать public static поле countries типа Map.
+2. В статическом блоке класса Solution инициализируй поле countries
+тестовыми данными согласно заданию.
+3. Класс IncomeDataAdapter должен реализовывать интерфейсы Customer и Contact.
+4. Класс IncomeDataAdapter должен содержать приватное поле data типа IncomeData.
+5. Класс IncomeDataAdapter должен содержать конструктор с параметром IncomeData.
+6. В классе IncomeDataAdapter реализуй методы интерфейсов
+Customer и Contact используя подсказки в виде комментариев в интерфейсах.
+*/
+
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+        Contact phone = new IncomeDataAdapter(new IncomeData() {
+            @Override
+            public String getCountryCode() {
+                return "UA";
+            }
+
+            @Override
+            public String getCompany() {
+                return "JavaRush Ltd";
+            }
+
+            @Override
+            public String getContactFirstName() {
+                return "Ivan";
+            }
+
+            @Override
+            public String getContactLastName() {
+                return "Ivanov";
+            }
+
+            @Override
+            public int getCountryPhoneCode() {
+                return 38;
+            }
+
+            @Override
+            public int getPhoneNumber() {
+                return 501234567;
+            }
+        });
+        System.out.println(phone.getPhoneNumber());
+        System.out.println(phone.getName());
+
+        Customer cus = (Customer) phone;
+        System.out.println(cus.getCompanyName());
+        System.out.println(cus.getCountryName());
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            String s = this.data.getContactLastName()
+                    .concat(", ")
+                    .concat(this.data.getContactFirstName());
+            return s;
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            return "+" + data.getCountryPhoneCode() + String.format(
+                    "%010d",
+                    data.getPhoneNumber()
+            ).replaceFirst(
+                    "(\\d{3})(\\d{3})(\\d{2})(\\d{2})",
+                    "($1)$2-$3-$4"
+            );
+        }
+
+        public String getPhoneNumberMaskFormater() {
+            String phoneNumWithCode = data.getCountryPhoneCode() +
+                    String.format("%010d", data.getPhoneNumber());
+            String returnString = "";
+            try {
+                MaskFormatter maskFormatter =
+                        new MaskFormatter("+##(###)###-##-##");
+                maskFormatter.setValueContainsLiteralCharacters(false);
+                returnString = maskFormatter.valueToString(phoneNumWithCode);
+            } catch (ParseException pe) {
+                pe.printStackTrace();
+            }
+            return returnString;
+        }
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //example UA
+
+        String getCompany();            //example JavaRush Ltd.
+
+        String getContactFirstName();   //example Ivan
+
+        String getContactLastName();    //example Ivanov
+
+        int getCountryPhoneCode();      //example 38
+
+        int getPhoneNumber();           //example 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}
+
+/*
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter {
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //example UA
+
+        String getCompany();            //example JavaRush Ltd.
+
+        String getContactFirstName();   //example Ivan
+
+        String getContactLastName();    //example Ivanov
+
+        int getCountryPhoneCode();      //example 38
+
+        int getPhoneNumber();           //example 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}*/
