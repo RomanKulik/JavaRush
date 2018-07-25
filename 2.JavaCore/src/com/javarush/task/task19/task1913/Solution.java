f7407978842762ece94ea406e18ccbf24cc59a77
RomanKulik <roman.ser.kulik@gmail.com>
2018-07-25 21:21:51 +0300

    RegEx link, Подмена System.out

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+Выводим только цифры
+В методе main подмени объект System.out
+написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить только цифры.
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку
+"it's 1 a 23 text 4 f5-6or7 tes8ting".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+
+https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream copyOut = System.out;
+
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(barr));
+
+        testString.printSomething();
+
+        System.setOut(copyOut);
+        System.out.println(barr.toString().
+                replaceAll("\\D", ""));
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
+
+/*public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}*/
