b7f90ff149a2c57d1fd4b90a140776a2fc48a175
RomanKulik <roman.ser.kulik@gmail.com>
2018-07-25 21:01:00 +0300

    Переопределение System.out с пояснением

--- 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -79,3 +79,17 @@ public class Solution {
         }
     }
 }
+
+/*public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}*/
