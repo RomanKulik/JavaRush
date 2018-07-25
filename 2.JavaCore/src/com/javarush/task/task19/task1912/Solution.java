028a7f7adcbf4675ba13c3a9ea49624cb425809e
RomanKulik <roman.ser.kulik@gmail.com>
2018-07-25 21:10:56 +0300

    Преопределение System.out replaceAll(te,??)

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+Ридер обертка 2
+В методе main подмените объект System.out
+написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна заменять все подстроки "te" на "??".
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream copyOut = System.out;
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(barr));
+        testString.printSomething();
+        System.setOut(copyOut);
+        System.out.println(barr.toString().replaceAll("te","??"));
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
+
+/*
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+*/
