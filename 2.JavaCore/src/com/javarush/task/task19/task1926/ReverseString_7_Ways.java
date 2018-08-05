25c2927e854a1f12195956d38ba50fc35d177872
RomanKulik <roman.ser.kulik@gmail.com>
2018-08-05 20:34:09 +0300

    ReverseString_7_Ways:StringBuffer.reverse,forr.str.toCharArray(),forrStringBuilder,recursiveReverse,reverseByStack,reverseWithXOR,reverseWithVar

--- 2.JavaCore/src/com/javarush/task/task19/task1926/ReverseString_7_Ways.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/ReverseString_7_Ways.java
@@ -3,9 +3,12 @@ package com.javarush.task.task19.task1926;
 import java.util.Stack;
 
 /**
+ * ReverseString_7_Ways:StringBuffer.reverse,forr.str.toCharArray(),forrStringBuilder,recursiveReverse,reverseByStack,reverseWithXOR,reverseWithVar
  * @author Sergey Lugovoy <svlugovoy@gmail.com> 05.02.2016.
+ * @author Практика применения XOR в программировании https://habr.com/post/183462/
+ * XOR or '^'   https://stackoverflow.com/questions/1991380/what-does-the-operator-do-in-java
  */
-public class ReverseString_5Ways {
+public class ReverseString_7_Ways {
     public static void main(String[] args) {
         String str = "I love Java";
         // 1
@@ -28,6 +31,11 @@ public class ReverseString_5Ways {
         System.out.println("4) " + newStr);
         // 5
         System.out.println("5) " + reverseByStack(str));
+        // 6
+        System.out.println("6) " + reverseWithXOR(str));
+        // 7
+        System.out.println("7) " + reverseWithVar(str));
+
     }
 
     static String recursiveReverse(String s) {
@@ -47,5 +55,29 @@ public class ReverseString_5Ways {
             sb.append(st.pop());
         }
         return sb.toString();
+    }
+
+    public static final String reverseWithXOR(String string) {
+        char[] array = string.toCharArray();
+        int length = array.length;
+        int half = (int) Math.floor(array.length / 2);
+        for (int i = 0; i < half; i++) {
+            array[i] ^= array[length - i - 1];
+            array[length - i - 1] ^= array[i];
+            array[i] ^= array[length - i - 1];
+        }
+        return String.valueOf(array);
+    }
+
+    public static final String reverseWithVar(String string) {
+        char[] array = string.toCharArray();
+        int length = array.length;
+        int half = (int) Math.floor(array.length / 2);
+        for (int i = 0; i < half; i++) {
+            char temp = array[i];
+            array[i] = array[length - i - 1];
+            array[length - i - 1] = temp;
+        }
+        return String.valueOf(array);
     }
 }
