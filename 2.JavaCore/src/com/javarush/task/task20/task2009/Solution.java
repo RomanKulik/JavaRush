75b699d33cd2e887f550a7079834165c1bd9b107
RomanKulik <roman.ser.kulik@gmail.com>
2020-06-04 19:21:53 +0300

    Серриализация static

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task20.task2009;
+
+import java.io.Serializable;
+
+/*
+Как сериализовать static?
+Как сериализовать static?
+Сделай так, чтобы сериализация класса ClassWithStatic была возможной.
+
+
+Требования:
+1. Класс ClassWithStatic должен существовать внутри класса Solution.
+2. Класс ClassWithStatic должен быть статическим.
+3. Класс ClassWithStatic должен быть публичным.
+4. Класс ClassWithStatic должен поддерживать интерфейс Serializable.
+*/
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "This is a static test string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
