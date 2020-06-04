fa37c36f69db1dabfe495d573902c811e526dc48
RomanKulik <roman.ser.kulik@gmail.com>
2020-06-04 20:55:05 +0300

    MinesweeperGame: Step 1

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,11 +3,4 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
-    private static final int SIDE = 9; // the field's size
-
-    @Override
-    public void initialize(){
-        setScreenSize(SIDE, SIDE);
-    }
 }
-
