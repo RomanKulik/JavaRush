afbd399d1becba649288467b98efec2c28c86048
RomanKulik <roman.ser.kulik@gmail.com>
2020-06-04 21:32:13 +0300

    MinesweeperGame: Step 4. Двумерный массив [y][x]

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,10 +4,21 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9; // the field's size
+    private GameObject[][] gameField = new GameObject[this.SIDE][this.SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int x = 0; x < this.SIDE; x++) {
+            for (int y = 0; y < this.SIDE; y++) {
+                this.gameField[x][y] = new GameObject(y, x);
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
     }
 }
 
