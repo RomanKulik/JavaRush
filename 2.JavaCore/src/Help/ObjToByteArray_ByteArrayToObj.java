67bf2e26914901d780a02e42c399e9ab05f6b5d2
RomanKulik <roman.ser.kulik@gmail.com>
2020-06-13 12:49:48 +0300

    Help: ObjToByteArray_ByteArrayToObj

---
+++ 2.JavaCore/src/Help/ObjToByteArray_ByteArrayToObj.java
@@ -0,0 +1,27 @@
+package Help;
+
+import java.io.*;
+
+public class ObjToByteArray_ByteArrayToObj {
+    public byte[] objToByteArray(Object o) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(o);
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Object byteArrayToObject(byte[] bytes) {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return ois.readObject();
+        } catch (IOException e) {
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
