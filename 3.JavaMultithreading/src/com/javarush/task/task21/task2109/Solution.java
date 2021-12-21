8d21ed659716ac03cf08ad9ed535aab31c4f12b6
RomanKulik <roman.ser.kulik@gmail.com>
2021-12-21 20:25:45 +0200

    Запретить клонирование: throw new CloneNotSupportedException();

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -2,7 +2,140 @@ package com.javarush.task.task21.task2109;
 
 /* 
 Запретить клонирование
+Запретить клонирование
+Разреши клонировать класс А
+Запрети клонировать класс B
+Разреши клонировать класс C
+Не забудь о методах equals и hashCode!
+
+
+Requirements:
+1. Класс A должен поддерживать интерфейс Cloneable.
+2. Класс B должен быть потомком класса A.
+3. При объявлении класса B не должно быть явно указано implements Cloneable.
+4. Метод clone в классе B должен быть переопределен таким образом,
+чтобы при попытке клонирования объекта класса B возникало исключение CloneNotSupportedException.
+5. Класс C должен быть потомком класса B.
+6. Клонирование объектов класса C должно завершаться успешно.
 */
+
+import java.util.Objects;
+
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null) return false;
+            if (o.getClass() != this.getClass()) return false;
+
+            A a = (A) o;
+            return i == a.i && j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null) return false;
+            if (!(o instanceof B)) return false;
+
+            if (!(super.equals(o))) return false;
+            return Objects.equals(name, ((B) o).name);
+        }
+    }
+
+    public static class C extends B {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return new C(getI(), getJ(), getName());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return super.equals(o);
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        C c = new C(1, 3, "");
+        C c1 = (C) c.clone();
+        System.out.println(c1);
+    }
+}
+
+/*
+package com.javarush.task.task21.task2109;
+
+/*
+Запретить клонирование
+Запретить клонирование
+Разреши клонировать класс А
+Запрети клонировать класс B
+Разреши клонировать класс C
+Не забудь о методах equals и hashCode!
+
+
+Requirements:
+1. Класс A должен поддерживать интерфейс Cloneable.
+2. Класс B должен быть потомком класса A.
+3. При объявлении класса B не должно быть явно указано implements Cloneable.
+4. Метод clone в классе B должен быть переопределен таким образом, чтобы при попытке клонирования объекта класса B возникало исключение CloneNotSupportedException.
+5. Класс C должен быть потомком класса B.
+6. Клонирование объектов класса C должно завершаться успешно.
+
 
 public class Solution {
     public static class A implements Cloneable {
@@ -46,3 +179,5 @@ public class Solution {
 
     }
 }
+
+ */
