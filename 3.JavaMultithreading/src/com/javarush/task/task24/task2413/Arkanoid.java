35ef5c51c4de2bb2b46e4bf8105222a7fe623e8c
RomanKulik <roman.ser.kulik@gmail.com>
2022-09-27 21:23:33 +0300

    Arkanoid 2

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,24 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int width;
+    private int height;
+
+    public int getWidth(){
+        return this.width;
+    }
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+
+
     public static void main(String[] args) {
 
     }
