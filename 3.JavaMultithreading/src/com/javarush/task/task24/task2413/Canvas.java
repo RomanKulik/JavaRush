00d5fda7ce910e0b4e7d1f657433618c11aff2e0
RomanKulik <roman.ser.kulik@gmail.com>
2022-09-28 14:16:42 +0300

    Arkanoid 9: Canvas

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,5 +1,38 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+    private int height;
+    private int width;
+    private char[][] matrix;
 
+    public int getHeight(){
+        return this.height;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public char[][] getMatrix(){
+        return this.matrix;
+    }
+
+    public void setMatrix(char[][] matrix){
+        this.matrix = matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.height = height;
+        this.width = width;
+
+        this.matrix = new char[this.height + 2][this.width + 2];
+    }
 }
