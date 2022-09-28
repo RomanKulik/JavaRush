3947b1c65b282ac79a2ed50f6993a74cc7da61db
RomanKulik <roman.ser.kulik@gmail.com>
2022-09-28 19:35:13 +0300

    Arkanoid 16: Stand

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,11 +1,36 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
+    private double speed; // скорость шарика
+    // направление движения по оси x:
+    // 1 - вправо, -1 - влево, 0 - начальное значение, стоим на месте
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
 
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
     }
 
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
 
     @Override
     public void draw(Canvas canvas){}
