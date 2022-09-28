de2ecf8e2d932f6418ff9334b4c3670052c8e017
RomanKulik <roman.ser.kulik@gmail.com>
2022-09-28 14:55:20 +0300

    Arkanoid 10: Canvas, setPoint, drawMatrix

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,15 +1,23 @@
 package com.javarush.task.task24.task2413;
 
+
+/**
+ * Что мы будем делать с Canvas?
+ * Мы будем рисовать на нем (в его матрице).
+ * Поэтому нам понадобятся два метода:
+ * void setPoint(double x, double y, char c)
+ * void drawMatrix(double x, double y, int[][] matrix, char c)
+ */
 public class Canvas {
     private int height;
     private int width;
     private char[][] matrix;
 
-    public int getHeight(){
+    public int getHeight() {
         return this.height;
     }
 
-    public void setHeight(int height){
+    public void setHeight(int height) {
         this.height = height;
     }
 
@@ -17,15 +25,15 @@ public class Canvas {
         return this.width;
     }
 
-    public void setWidth(int width){
+    public void setWidth(int width) {
         this.width = width;
     }
 
-    public char[][] getMatrix(){
+    public char[][] getMatrix() {
         return this.matrix;
     }
 
-    public void setMatrix(char[][] matrix){
+    public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
 
@@ -35,4 +43,47 @@ public class Canvas {
 
         this.matrix = new char[this.height + 2][this.width + 2];
     }
+
+    /**
+     * Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
+     * В методе надо:
+     * а) округлить x и y до целых чисел, используя метод Math.round()
+     * б) занести в matrix[y][x] значение с
+     * в) ничего не делать, если x < 0 или y < 0 или
+     * y >= matrix.length или x >= matrix[0].length
+     */
+    void setPoint(double x, double y, char c) {
+        int rx = (int) Math.round(x);
+        int ry = (int) Math.round(y);
+
+        if (!(rx < 0 || ry < 0 ||
+                ry > this.matrix.length ||
+                rx >= this.matrix[0].length)) {
+            this.matrix[ry][rx] = c;
+        }
+    }
+
+    /**
+     * Второй метод - drawMatrix копирует переданную ему картинку (матрицу)
+     * в матрицу Canvas.
+     * И не просто копирует, а начиная с координат x, y
+     * В методе надо:
+     * а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки
+     * б) если значение ячейки элемента [i][j] полученной матрицы не равно 0,
+     * то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
+     * setPoint(x+j, y+i, c)
+     */
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int rx = (int)Math.round(x);
+        int ry = (int)Math.round(y);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] !=0 ){
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
+
 }
