e9e48b9a3051047d94ccd4bcef7a246a8c6420cc
RomanKulik <roman.ser.kulik@gmail.com>
2022-10-03 13:31:56 +0300

    Own Marker Interface + create instance new Intarface()

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void method1(){}
+    public void method2(){}
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/UnsupportedInterfaceMarkerException.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task24.task2401;
+
+public class UnsupportedInterfaceMarkerException extends Exception {
+
+}
