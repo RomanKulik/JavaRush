341b453753c9b89fe4ce2d9d1c20ac9d318478dc
RomanKulik <roman.ser.kulik@gmail.com>
2022-10-03 21:30:29 +0300

    Black box: super.someAction(); Solution.this.someAction(); return param > 0 ? Solution.this : this

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/FirstClass.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task24.task2405;
+
+public abstract class FirstClass implements Action {     //first implementation
+
+    protected FirstClass() {
+        Solution.countActionObjects++;
+    }
+
+    public void someAction() {
+        System.out.println("class FirstClass, method someAction");
+    }
+
+    public abstract Action getDependantAction();
+}
+
+/*
+package com.javarush.task.task24.task2405;
+
+public abstract class FirstClass implements Action {     //first implementation
+
+    protected FirstClass() {
+        Solution.countActionObjects++;
+    }
+
+    public void someAction() {
+        System.out.println("class FirstClass, method someAction");
+    }
+
+    public abstract Action getDependantAction();
+}
+
+ */
